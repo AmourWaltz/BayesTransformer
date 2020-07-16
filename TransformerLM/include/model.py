3458778889cc84776f53affa0061a90d554a1ada
SharpWaltz <1276180641@qq.com>
2020-07-16 20:54:55 +0800

    7.16

--- TransformerLM/include/model.py
+++ TransformerLM/include/model.py
@@ -7,13 +7,6 @@ import torch.nn.functional as func
 
 # Scaled dot-product attention mechanism.
 class ScaledDotProductAttention(nn.Module):
-    """
-    An attention function can be described as a query and a set of key-value pairs to an output,
-    where the query, keys, values, and output are all vectors.
-    The output is computed as a weighted sum of the values,
-    where the weight assigned to each value is computed
-    by a compatibility of the query with the corresponding key.
-    """
     def __init__(self, attention_dropout=0.0):
         super(ScaledDotProductAttention, self).__init__()
         self.dropout = nn.Dropout(attention_dropout)
@@ -51,12 +44,6 @@ class ScaledDotProductAttention(nn.Module):
 
 # Multi-head attention architecture.
 class MultiHeadAttention(nn.Module):
-    """
-    We found it beneﬁcial to linearly project the queries, keys and values h times with different,
-    learned linear projections to d_k , d_k and d_v dimensions, respectively.
-    We then perform the attention function in parallel, yielding d_v-dimensional output values.
-    These are concatenated and once again projected, resulting in the ﬁnal values,
-    """
     def __init__(self, model_dim=512, num_heads=8, dropout=0.0):
         super(MultiHeadAttention, self).__init__()
         self.dim_per_head = model_dim // num_heads
@@ -154,14 +141,6 @@ def sequence_mask(seq):
 
 # Compute the positional encodings once in trigonometric space.
 class PositionalEncoding(nn.Module):
-    """
-    Where pos is the position and i is the dimension.
-    That is, each dimension of the positional encoding corresponds to a sinusoid.
-    The wavelengths form a geometric progression from 2π to 10000 · 2π.
-    We chose this function because we hypothesized
-    it would allow the model to easily learn to attend by relative positions,
-    since for any ﬁxed offset k, PE pos+k can be represented as a linear function of PE pos .
-    """
     def __init__(self, d_model, max_seq_len):
         """
         :param d_model: sclar, model dimension: 512 in Attention Is All You Need
@@ -216,12 +195,6 @@ class PositionalEncoding(nn.Module):
 
 # Implements FFN equation.
 class PositionalWiseFeedForward(nn.Module):
-    """
-    Two linear transformer and one ReLU. In addition to attention sub-layers,
-    each of the layers in our encoder and decoder contains a fully connected feed-forward network,
-    which is applied to each position separately and identically.
-    This consists of two linear transformations with a ReLU activation in between.
-    """
     def __init__(self, model_dim=512, ffn_dim=2048, dropout=0.0):
         super(PositionalWiseFeedForward, self).__init__()
         self.w1 = nn.Conv1d(model_dim, ffn_dim, 1)
